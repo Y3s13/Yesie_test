d594810c08fb8705942c52e01f33ed54c97dee50
Y3s13 <144732124+Y3s13@users.noreply.github.com>
2023-10-05 14:14:16 -0600

    Update styles.cs

--- styles.cs
+++ styles.cs
@@ -1,1 +1,22 @@
+body{
+  background-color:#ffffff;
+  margin-left: 30px;
+  margin-right: 30px;
+  }
+h1{
+  text-align: center;
+  padding: 30px;
+  background-color:#ffffff;
+  border-radius: 15px;
+  margin: 15px;
+}
+div{
+  background-color:#7ebbbd;
+  padding: 30px;
+  border-radius: 15px;
+  margin: 15px;
+}
+p{
+  text-align: center;
+}
 
