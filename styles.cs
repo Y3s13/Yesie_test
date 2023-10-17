02dc18fce4079a095fe1d20dd46f2603b270af61
Y3s13 <144732124+Y3s13@users.noreply.github.com>
2023-10-17 13:36:33 -0600

    Update styles.cs

--- styles.cs
+++ styles.cs
@@ -4,12 +4,11 @@ body{
   margin-right: 10px;
   }
 h1{
-  text-align: center;
-  padding-left: 100px;
-  background-color:#7ebbbd;
-  border-radius: 50px;
+ background-color:#7ebbbd;
+  border-radius: 15px;
   margin: 15px;
   width: 60%;
+  float: left;
 }
 div{
   background-color:#7ebbbd;
