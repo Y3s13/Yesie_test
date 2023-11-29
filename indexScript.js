518f88c36a19408186927ef2576901d6f308ab6e
Y3s13 <144732124+Y3s13@users.noreply.github.com>
2023-11-29 13:40:54 -0700

    Update indexScript.js

--- indexScript.js
+++ indexScript.js
@@ -1,6 +1,11 @@
+let nameOfElement;
+nameOfElement = "demo";
+console.log("i hope you enjoy learning JavaScript") 
+
 alert("Welcome to my page");
 
 console.log("would you like to continue?");
+
 function changeText(){
   document.getElementById("demo").innerHTML = "This is a bucket.";
 }
