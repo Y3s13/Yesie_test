946a73f95cb46c9cbcc761d4aaf4b413c9357aeb
Y3s13 <144732124+Y3s13@users.noreply.github.com>
2023-12-08 09:27:05 -0700

    Create otherScript.js

---
+++ otherScript.js
@@ -0,0 +1,8 @@
+window.alert("John doe");
+window.alert("Jane doe");
+
+ console.log("would you like to continue?");
+
+function changeText(){
+   document.getElementByid("demo").innerHTML = "Hello";
+}
