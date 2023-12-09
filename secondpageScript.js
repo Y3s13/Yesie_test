f39ddda5be27849098f2d768b71922fe8b3a9ef1
Y3s13 <144732124+Y3s13@users.noreply.github.com>
2023-12-09 16:25:25 -0700

    Update secondpageScript.js

--- secondpageScript.js
+++ secondpageScript.js
@@ -1,6 +1,4 @@
-window.alert
-let firstName = "John"
-let lastName = "Doe";
+window.alert("this page is about how i got into sewing and what im doing with sewing now a days");
 
  console.log("would you like to continue?");
 
